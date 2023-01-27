0bba8620aaa8f96866f0510cff8caf963dc16090
juan <sol201598@uvg.edu.gt>
2023-01-27 08:49:48 -0600

    Uniendo todo el programa dejando principal funcional

--- Calculadora.java
+++ Calculadora.java
@@ -1,54 +1,67 @@
-public class Calculadora<T> implements IPostfixCalculator {
-    private StackHandmade<int> numeros = new StackHandmade<int>();
+import java.util.ArrayList;
+
+public class Calculadora<T> implements IPostfixCalculator<T> {
+
 
     public Calculadora() {
 
     }
 
-    public int Calculate(java.util.ArrayList<T> postfix_expression) throws Exception{
+    /**
+     * Es el metodo que calcula
+     * @param postfix_expression calculo de enteros en postfix
+     * @return resultado
+     * @throws Exception
+     */
+    @Override
+    public int Calculate(ArrayList<T> postfix_expression) throws Exception{
+
         int respuesta = 0;
-        try{
-            for (T u : postfix_expression){
-                if (u instanceof String){
-                    char v = ((String) u).charAt(0);
+        StackHandmade<Integer> numeros = new StackHandmade<Integer>();
+
+
+        for (T u : postfix_expression){
+
+            char v = (char) u;
+            if ( Character.isDigit(v) ){
+                numeros.push(Integer.parseInt(String.valueOf(v)));
+            }
+            switch (v){
+                case '+':
+                    respuesta = numeros.pull() + numeros.pull();
+                    numeros.push(respuesta);
+                    break;
+
+                case '-':
+                    respuesta = numeros.pull() - numeros.pull();
+                    numeros.push(respuesta);
+                    break;
+
+                case '*':
+                    respuesta = numeros.pull() * numeros.pull();
+                    numeros.push(respuesta);
+                    break;
+
+                case '/':
+                    respuesta = numeros.pull() / numeros.pull();
+                    numeros.push(respuesta);
+                    break;
+
+                case ' ':
+                    break;
+
+                default:
+                   // System.out.println("caracterinvalido en la linea, no se tomara en cuenta en el calculo");
+
+                    //dar resultado
+
+
                 }
-                else{
-                    char v = (char) u;
-                }
-                if ( v.isDigit() ){
-		            numeros.push(Integer.parseInt(v));
-                }
-                switch (v){
-                    case "+":
-                        // algo
-                        break;
 
-                    case "-":
-                        // algo
-                        break;
-
-                    case "*":
-                        // algo
-                        break;
-
-                    case "/":
-                        // algo
-                        break;
-
-                    default:
-                        System.out.println("caracterinvalido en la linea, no se tomara en cuenta en el calculo")
-
-                        //dar resultado
-
-
-                }
 
             }
-        }
-        catch (){
 
-        }
-        return respuesta;
+        return numeros.pull();
     }
 
 
@@ -56,3 +69,4 @@ public class Calculadora<T> implements IPostfixCalculator {
 
 
 }
+
--- Principal.java
+++ Principal.java
@@ -1,26 +1,34 @@
 import java.util.*;
 
 
-public class Main {
+public class Principal{
     public static void main(String[] args) {
         Leo leo = new Leo();
         int i = 0;
         IPostfixCalculator calculadora = new Calculadora();
         // leer el archivo
-        ArrayList<String> operaciones = leo.leerArchivo("ejemplo.txt");
+        ArrayList<String> operaciones = leo.leerArchivo("datos.txt");
         //se recorre cada linea del archivo
         for (String operacion : operaciones) {
             i ++;
             //se calcula expresión en formato Postfix de la linea i empieza en 1
             System.out.println("operacion " + i + ":");
+
             try{
-                System.out.println(calculadora.Calculate(operacion));
+                System.out.println(calculadora.Calculate(convertStringToCharList(operacion)));
             }
             catch (Exception ex){
                 System.out.println("Error operacion invalida");
             }
 
         }
+    }
+    public static ArrayList<Character> convertStringToCharList(String str){
+        ArrayList<Character> chars = new ArrayList<>();
+        for (char ch : str.toCharArray()) {
+            chars.add(ch);
+        }
+        return chars;
     }
 }
 
